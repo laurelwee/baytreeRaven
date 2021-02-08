a273a9cc015b14586122596cb3aa1b63c69e6ff0
laurelwee <laurelwee@bren.ucsb.edu>
2021-02-08 15:08:33 -0800

    added plastic_theme

--- R/laurel_theme.R
+++ R/laurel_theme.R
@@ -16,4 +16,22 @@ laurel_theme <- function() {
   )
 }
 
+plastic_theme <- function() {
+  theme(
+    panel.background = element_rect(fill = "lightcyan"),
+    panel.grid.major.x = element_line(colour = "darkblue", linetype = 3, size = 0.5),
+    panel.grid.minor.x = element_blank(),
+    panel.grid.major.y =  element_line(colour = "darkblue", linetype = 3, size = 0.5),
+    axis.text = element_text(colour = "midnightblue"),
+    axis.title = element_text(colour = "turquoise4"),
+    axis.title.y = element_text(angle = 0),
+
+
+    plot.title = element_text(family = font,size = 16, face = 'bold', color= textcolor, v=3),
+    axis.title = element_text(family = font, size = 10, face = 'bold', color= textcolor),
+    axis.text = element_text(family = font,  size = 8, face = "italic", color= textcolor),
+    legend.text = element_text(family = font,size = 8, face = 'bold', colour = textcolor),
+
+  )
+}
 
